309654258493749d43e19673bc39ec4e101377f2
HinataOe <hinatan430_6521@yahoo.co.jp>
2021-01-27 10:32:29 +0000

    php/laravel 11 error

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -5,9 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MyProfile</title>
+        <title>MyProfileCreate</title>
     </head>
     <body>
-        <h1>Myプロフィール作成画面</h1>
+        <h1>My Profile 作成画面</h1>
     </body>
 </html>
