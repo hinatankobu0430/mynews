38e49f0cf1c664109a4f7edf2290b1ee2432b218
HinataOe <hinatan430_6521@yahoo.co.jp>
2021-01-28 01:40:40 +0000

    php/laravel 11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -9,5 +9,21 @@
     </head>
     <body>
         <h1>My Profile 作成画面</h1>
+@extends('layouts.admin')
+
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'プロフィールの新規作成')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My　プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
     </body>
 </html>
